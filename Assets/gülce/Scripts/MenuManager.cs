dff96a982b3d4389fa28db3d136b82dbe799a64c
Gulce Capak <gulcecapak@gmail.com>
2023-07-06 12:02:14 +0300

    Update MenuUI

---
+++ Assets/gülce/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject mainMenuPanel;
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Button Clicked!");
+
+        // Settings panelini aç
+        settingsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+
+    }
+
+    public void OnBackButtonClick()
+    {
+        // Settings panelini kapatıp main menu panelini aç
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+}
--- Assets/gülce/Scripts/MenuUIButtons.cs
+++ Assets/gülce/Scripts/MenuUIButtons.cs
@@ -9,23 +9,22 @@ using TMPro;
 public class MenuUIButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     // Kayma Animasyonu
-    public Transform targetPosition;
+    public Transform targetPosition; // butonların target pozisyonları
     public float duration = 1f;
-    public float delay = 0.5f;
+    public float delay = 0.5f; // butonların arasındaki süre 
 
-    //Şekil ve renk değiştirme
-    // public TextMeshProUGUI itemText;
+    //Şekil değiştirme
     private Button button;
     private Vector3 originalScale;
 
-    public Color normalColor;
-    public Color selectedColor;
+    //Renk değiştirme
+    //private Color normalColor;
+    //private Color selectedColor;
 
-    public float selectedScale = 1.2f;
-    public float animationDuration = 0.2f;
 
-    private bool isSelected = false;
-    private Vector3 initialScale;
+
+    public float selectedScale = 1.2f; // buton seçiliykenki scale
+    public float animationDuration = 0.2f; // animasyon süresi
 
 
 
@@ -36,19 +35,12 @@ public class MenuUIButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
         button = GetComponent<Button>();
         originalScale = transform.localScale;
+        //normalColor = button.colors.normalColor;
+        //selectedColor = button.colors.highlightedColor;
 
-        // Butonun üzerine gelindiğinde büyüme animasyonunu başlat
-        //button.OnPointerEnter.AddListener(OnPointerEnter);
 
-        // Butonun üzerinden çıkıldığında küçülme animasyonunu başlat
-        //button.OnPointerExit.AddListener(OnPointerExit);
 
         button.onClick.AddListener(OnButtonClick);
-
-
-
-        //initialScale = transform.localScale;
-        //SetItemColor(normalColor);
     }
 
     // Kayma Animasyonu
@@ -63,12 +55,24 @@ public class MenuUIButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     {
         // Büyüme animasyonunu başlat
         transform.DOScale(selectedScale, animationDuration);
+
+        // Renk değişikliği için DOTween kullanarak geçiş yap
+        //var colorBlock = button.colors;
+        //colorBlock.normalColor = selectedColor;
+        //button.colors = colorBlock;
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         // Küçülme animasyonunu başlat
         transform.DOScale(originalScale, animationDuration);
+
+        // Renk değişikliği için DOTween kullanarak geçiş yap
+        //var colorBlock = button.colors;
+        //colorBlock.normalColor = normalColor;
+        //button.colors = colorBlock;
+
     }
 
     private void OnButtonClick()
@@ -76,46 +80,6 @@ public class MenuUIButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         Debug.Log("Button Clicked!");
     }
 
-
-
-
-    //public void SetSelected(bool selected)
-    //{
-    //    isSelected = selected;
-
-    //    if (isSelected) // MenuItem seçildiyse
-    //    {
-    //        // Büyüme animasyonunu başlat
-    //        transform.DOScale(Vector3.one * selectedScale, animationDuration);
-
-    //        // Renk değişikliği için DOTween kullanarak geçiş yap
-    //        button.image.DOColor(selectedColor, animationDuration);
-    //    }
-    //    else // MenuItem seçili değilse
-    //    {
-    //        // Küçülme animasyonunu başlat
-    //        transform.DOScale(Vector3.one, animationDuration);
-
-    //        // Renk değişikliği için DOTween kullanarak geçiş yap
-    //        button.image.DOColor(normalColor, animationDuration);
-    //    }
-    //}
-
-
-
-
-
-    //private void AnimateSelection(float targetScale)
-    //{
-    //    if (isSelected)
-    //    {
-    //        transform.DOScale(initialScale * targetScale, animationDuration);
-    //    }
-    //    else
-    //    {
-    //        transform.DOScale(initialScale, animationDuration);
-    //    }
-    //}
 }
 
 
