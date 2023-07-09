fc75d5cfaeccab6cdf811f3e4be9af13277198f4
Gulce Capak <gulcecapak@gmail.com>
2023-07-10 00:25:32 +0300

    Update Scripts-Added Loading Scene images

---
+++ Assets/gülce/Scripts/Menu UI Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LevelManager : MonoBehaviour
+{
+    public Button[] buttons;
+    public GameObject levelMenuFirstButton;
+
+
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1); // Oyunda açılan son seviyenin indeksini al, varsayılan olarak 1 i ayarla
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false; //Butonları devre dışı bırak
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true; //Açılan seviyelere kadar olan butonları tıklanabilir yap
+        }
+    }
+    private void Start()
+    {
+        //Seçili nesneyi temizle
+        EventSystem.current.SetSelectedGameObject(null);
+        //Yeni bir seçili nesne belirle
+        EventSystem.current.SetSelectedGameObject(levelMenuFirstButton);
+    }
+
+    public void OpenLevel(int levelId)
+    {
+        string levelName = "LEVEL" + levelId; //Seviye adını belirle
+        SceneManager.LoadScene(levelName); //Seviyeyi yükle
+    }
+}
---
+++ Assets/gülce/Scripts/Menu UI Scripts/MenuManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MenuManager : MonoBehaviour
+{
+    public string sceneName;
+    public GameObject settingsPanel;
+    public GameObject mainMenuPanel;
+    public GameObject creditsPanel;
+    public GameObject levelsPanel;
+
+
+    public GameObject settingsMenuButton, creditsMenuButton, creditsMenuBackButton;
+
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Done!");
+    }
+
+    public void OpenLoadGame()
+    {
+        // Levels Paneli aç
+        levelsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+
+    }
+
+    public void OpenSettings()
+    {
+        // Settings panelini aç
+        settingsPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        // Settings panelini kapatıp main menu panelini aç
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new selected object
+        EventSystem.current.SetSelectedGameObject(settingsMenuButton);
+    }
+
+    public void OpenCredits()
+    {
+        // Credits panelini aç
+        creditsPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new selected object
+        EventSystem.current.SetSelectedGameObject(creditsMenuBackButton);
+    }
+    public void CloseCredits()
+    {
+        // Credits panelini aç
+        creditsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+
+
+        //clear selected object
+        EventSystem.current.SetSelectedGameObject(null);
+        //set a new selected object
+        EventSystem.current.SetSelectedGameObject(creditsMenuButton);
+    }
+
+}
