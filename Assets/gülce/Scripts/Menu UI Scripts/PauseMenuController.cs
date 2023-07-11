9c6a7e87114a9b4862d151e66a6f6cd7f24799ce
Gulce Capak <gulcecapak@gmail.com>
2023-07-11 13:52:43 +0300

    Update PauseMenu

--- Assets/gülce/Scripts/Menu UI Scripts/PauseMenuController.cs
+++ Assets/gülce/Scripts/Menu UI Scripts/PauseMenuController.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class PauseMenuController : MonoBehaviour
 {
+    MenuInputManager menuInputManager;
     public GameObject pauseMenuPanel;
     public static bool gameIsPaused;
 
+
     public string sceneName;
 
+    void Start()
+    {
+        menuInputManager = MenuInputManager.Instance;
+    }
 
     private void Update()
     {
@@ -20,24 +27,27 @@ public class PauseMenuController : MonoBehaviour
     public void Resume()
     {
         pauseMenuPanel.SetActive(false);
-        //Time.timeScale = 1f;
+        menuInputManager.GetCurrentPanel();
+        Time.timeScale = 1f;
         gameIsPaused = false;
     }
 
     public void Restart()
     {
         pauseMenuPanel.SetActive(false);
-        //Time.timeScale = 1f;
+        menuInputManager.GetCurrentPanel();
+        Time.timeScale = 1f;
         gameIsPaused = false;
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-       
+
     }
 
     public void Pause()
     {
         pauseMenuPanel.SetActive(true);
-        //Time.timeScale = 0f;
+        menuInputManager.GetCurrentPanel();
+        Time.timeScale = 0f;
         gameIsPaused = true;
     }
 
@@ -50,6 +60,7 @@ public class PauseMenuController : MonoBehaviour
             {
                 Resume();
                 Restart();
+
             }
             else
             {
@@ -61,6 +72,6 @@ public class PauseMenuController : MonoBehaviour
     public void LoadScene()
     {
         SceneManager.LoadScene(sceneName);
-
+        menuInputManager.GetCurrentPanel();
     }
 }
