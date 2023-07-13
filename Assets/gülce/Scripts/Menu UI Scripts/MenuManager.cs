c948577715c3a72f22063564e4d36eb2f3ae5851
Gulce Capak <gulcecapak@gmail.com>
2023-07-13 13:56:00 +0300

    Added Button and Menu Sounds
    Buttons sounds
    Main menu music
    Loading Panels GamePad-Keyboard
    GameOver scene panel and buttons

--- Assets/gülce/Scripts/Menu UI Scripts/MenuManager.cs
+++ Assets/gülce/Scripts/Menu UI Scripts/MenuManager.cs
@@ -4,33 +4,44 @@ using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+
 
 public class MenuManager : MonoBehaviour
 {
     MenuInputManager menuInputManager;
+    MenuUIButtons menuUIButtons;
     public string sceneName;
     public GameObject settingsPanel;
     public GameObject mainMenuPanel;
     public GameObject creditsPanel;
     public GameObject levelsPanel;
+    public GameObject gamepadLoadingScreen;
+    public GameObject keyboardLoadingScreen;
 
+    public Slider slider;
 
     public GameObject settingsMenuButton, creditsMenuButton, creditsMenuBackButton;
 
     void Start()
     {
         menuInputManager = MenuInputManager.Instance;
+        menuUIButtons = FindObjectOfType<MenuUIButtons>();
     }
 
-    public void StartGame()
+    public void NewGame(int levelId)
     {
-        SceneManager.LoadScene(sceneName);
+        menuUIButtons.OnPointerEnter();
+        string levelName = "LEVEL" + levelId; //Seviye adını belirle
+        StartCoroutine(LoadAsync(levelName));
     }
 
     public void Quit()
     {
         Application.Quit();
         Debug.Log("Quitting Done!");
+        menuUIButtons.OnPointerEnter();
     }
 
     public void OpenLoadGame()
@@ -90,7 +101,7 @@ public class MenuManager : MonoBehaviour
     }
     public void CloseCredits()
     {
-        // Credits panelini aç
+        // Credits panelini kapat
         creditsPanel.SetActive(false);
         settingsPanel.SetActive(false);
         mainMenuPanel.SetActive(true);
@@ -102,5 +113,38 @@ public class MenuManager : MonoBehaviour
         EventSystem.current.SetSelectedGameObject(creditsMenuButton);
         menuInputManager.GetCurrentPanel();
     }
+    
+    IEnumerator LoadAsync(string levelName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelName); //Seviyeyi yükle
+
+        gamepadLoadingScreen.SetActive(true); //bunun yerine SetActiveLoadingScreen() gelicek
+
+        while (operation.isDone == false)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+
+    }
+
+    //public void SetActiveLoadingScreen()
+    //{
+    //    if ()  //hangi kıoşul gelicek sor
+    //    {
+    //        //gamepad kullanılıyorsa
+    //        gamepadLoadingScreen.SetActive(true);
+    //        keyboardLoadingScreen.SetActive(false);
+    //    }
+    //    else
+    //    {
+    //        //keyboard kullanılıyorsa
+    //        gamepadLoadingScreen.SetActive(false);
+    //        keyboardLoadingScreen.SetActive(true);
+    //    }
+
 
 }
